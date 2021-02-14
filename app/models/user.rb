2d055c84c8cee55c99ed15c522c2a668610407ab
Nico Hahn <hahnni@student.itech-bs14.de>
2021-02-14 01:37:13 +0100

    prepared user registration

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+
+  has_secure_password
+
+  validates :email,
+            format: { with: /^(.+)@(.+)$/, message: "Email invalid" },
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 4, maximum: 254 }
+
+
+end
