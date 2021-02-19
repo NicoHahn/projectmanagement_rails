3e493c60df61140f7c5a63f164001055d41665b1
Nico Hahn <6137959-nicohahn@users.noreply.gitlab.com>
2021-02-19 15:37:56 +0100

    created company controller, model & migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email,
-            format: { with: /^(.+)@(.+)$/, message: "Email invalid" },
+            format: { with: /(.+)@(.+)/, message: "Email invalid" },
             uniqueness: { case_sensitive: false },
             length: { minimum: 4, maximum: 254 }
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'companies/new'
+  get 'companies/create'
+  get 'companies/update'
+  get 'companies/delete'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:new, :create]
