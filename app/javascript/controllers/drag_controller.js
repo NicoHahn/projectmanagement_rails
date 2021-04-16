0351bdae50c5f337c7aceddf60b763dd4ddbc56d
Nico Hahn <6137959-nicohahn@users.noreply.gitlab.com>
2021-04-16 19:02:10 +0200

    damn sortablejs is smart

--- app/javascript/controllers/drag_controller.js
+++ app/javascript/controllers/drag_controller.js
@@ -12,35 +12,11 @@ import Sortable from "sortablejs"
 
 export default class extends Controller {
   connect() {
-    console.log("test1");
     this.sortable = Sortable.create(this.element, {
       group: 'shared',
+      animation: 150,
       onEnd: this.end.bind(this)
     })
-    new Sortable(planned, {
-      group: 'shared', // set both lists to same group
-      animation: 150
-    });
-    new Sortable(to_do, {
-      group: 'shared', // set both lists to same group
-      animation: 150
-    });
-    new Sortable(in_progress, {
-      group: 'shared', // set both lists to same group
-      animation: 150
-    });
-    new Sortable(in_review, {
-      group: 'shared', // set both lists to same group
-      animation: 150
-    });
-    new Sortable(approved, {
-      group: 'shared', // set both lists to same group
-      animation: 150
-    });
-    new Sortable(done, {
-      group: 'shared', // set both lists to same group
-      animation: 150
-    });
   }
   end(event) {
     console.log(this.element);
